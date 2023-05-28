4590c87e33b9cf1415999e0e23cf2707de60d5af
Rishi Musuvathi <rishimusuvathi03@gmail.com>
2023-05-28 22:19:35 +0530

    added user validation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 import "./App.css";
 
 import Users from "./User /pages/Users";
-import NewPlace from "./places/pages/place";
+import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
+const NewPlace = () => {
+  return (
+    <form className="place-form">
+      <Input
+        elements="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please Enter a valid title."
+      />
+    </form>
+  );
+};
+
+export default NewPlace;
