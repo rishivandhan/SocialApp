25920d4ac7adb7194912c3e19ff23941fac71361
Rishi Musuvathi <rishimusuvathi03@gmail.com>
2023-06-08 11:08:44 +0530

    input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import "./NewPlace.css";
 
 const NewPlace = () => {
@@ -10,7 +11,7 @@ const NewPlace = () => {
         elements="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please Enter a valid title."
       />
     </form>
